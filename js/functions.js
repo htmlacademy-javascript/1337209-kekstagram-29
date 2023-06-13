40201944595f84837c4e186f101268d94db07a16
Schiktor <jogabonito@list.ru>
2023-06-13 23:13:05 +0300

    Правки

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,12 @@
 // задача №1
-/*
 const toStr = function (str, numb) {
-  console.log (str.length <= numb);
+  return (str.length <= numb);
 };
 
 toStr('привет', 6);
-*/
 
 // задача №2
-/*
+
 const chekOnPolydrome = (str) => {
   str = str.replaceAll(' ', '').toLowerCase();
   const newStr = str;
@@ -21,14 +19,8 @@ const chekOnPolydrome = (str) => {
 
   if (str === resultStr) {
     return true;
-  }return false;
+  } else {
+    return false;
+  }
 };
-chekOnPolydrome('д овоД'); */
-
-// задача №2, 2-ой вариант
-
-// const chekOnPolydrome = function (str) {
-//   return str.replaceAll(' ', '').toLowerCase() === str.replaceAll(' ', '').toLowerCase().split('').reverse('').join('');
-// };
-
-// chekOnPolydrome('ДоВод');
+chekOnPolydrome('д овоД');
