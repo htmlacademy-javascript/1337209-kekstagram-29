c34690e2b878a986a5e994a81defabc32f321425
Schiktor <jogabonito@list.ru>
2023-06-15 09:27:12 +0300

    Исправляет ошибки в дз

--- js/functions.js
+++ js/functions.js
@@ -1,26 +1,19 @@
 // задача №1
-const toStr = function (str, numb) {
+const cheсkStrLength = function (str, numb) {
   return (str.length <= numb);
 };
 
-toStr('привет', 6);
+cheсkStrLength('привеsт', 6);
 
 // задача №2
 
 const chekOnPolydrome = (str) => {
   str = str.replaceAll(' ', '').toLowerCase();
-  const newStr = str;
   let resultStr = '';
-  for (let i = newStr.length - 1; i >= 0; i--) {
+  for (let i = str.length - 1; i >= 0; i--) {
     resultStr += str[i];
   }
-
-  resultStr = resultStr.replaceAll(' ', '').toLowerCase();
-
-  if (str === resultStr) {
-    return true;
-  } else {
-    return false;
-  }
+  return str === resultStr;
 };
+
 chekOnPolydrome('д овоД');
